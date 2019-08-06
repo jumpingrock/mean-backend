a13eb055e9bf8f39be0f8d0a961a62f1620f2e82
jumpingrock <kennethyeongky@gmail.com>
2019-08-07 00:01:58 +0800

    success in sending msg to front-end

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/api/posts',(req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*Origin, X-Request-Width, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+})
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added Successfully!'
+    });
+})
+
+app.get('/api/posts',(req, res, next) => {
     const posts = [
         {id: 'kenneth', 
         title: 'First server-side post',
-        content: 'this is coming from the server'
+        content: 'This is coming from the server'
         },
         {id: 'keen', 
         title: 'Second server-side post',
