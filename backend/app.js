17fef90ad25e2cbdec34e8d1ede86e457d6c4df6
jumpingrock <kennethyeongky@gmail.com>
2019-08-08 01:28:41 +0800

    added mongodb but cant connect to mongo shell via cli, using powershell now

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
+//mongo "mongodb+srv://cluster0-qs02p.mongodb.net/test" --username kenneth
+//Yz9CupOcdi2NN0P3
+mongoose.connect('mongodb+srv://kenneth:Yz9CupOcdi2NN0P3@cluster0-qs02p.mongodb.net/node-angular?retryWrites=true&w=majority')
+    .then(() => { //async db connection using mongoose app
+        console.log('Connected to mongoDB!');
+    })
+    .catch(() => { //error log if db connection fail
+        console.log('Connection failed!!');
+    })
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+app.use((req, res, next) => {// cors allow for sending of url msg using different localhost
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
     res.setHeader("Access-Control-Allow-Headers", "*Origin, X-Request-Width, Content-Type, Accept");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
     next();
 })
 
 app.post('/api/posts', (req, res, next) => {
-    const post = req.body;
+    // const post = req.body;
+    
+    const post = new Post({ // Post model is from app.js where it is hooked up to mongoose
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
+    post.save(); //this will push data into mongodb atlas via mongoose 
+                //collection will be named after your pural form of your model name
     res.status(201).json({
         message: 'Post added Successfully!'
     });
---
+++ readme.md
@@ -0,0 +1,11 @@
+Welcome! 
+
+This is the back end to my MEAN-Stack repo
+
+Tech used are NodeJs and MongoDB Atlas
+
+As of right now server access is limited by IP Address 
+
+But everything else should still work if you plug in your own DB!
+
+Kenneth
