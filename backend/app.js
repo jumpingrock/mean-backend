5088fff6eb5caa81eec00105abd3cb40863fbbfd
jumpingrock <kennethyeongky@gmail.com>
2019-08-19 01:02:24 +0800

    added file type validator

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 //mongo "mongodb+srv://cluster0-qs02p.mongodb.net/node-angular" --username kenneth
@@ -24,78 +24,7 @@ app.use((req, res, next) => {// cors allow for sending of url msg using differen
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
     next();
 })
+app.use('/api/posts', postsRoutes);
 
-app.post('/api/posts', (req, res, next) => {
-    // const post = req.body;
-    
-    const post = new Post({ // Post model is from app.js where it is hooked up to mongoose
-        title: req.body.title,
-        content: req.body.content
-    });
-    // console.log(res);
-    post.save().then(postCreated => {
-        console.log(postCreated);
-        res.status(201).json({
-            message: 'Post added Successfully!',
-            postId: postCreated._id
-        }); //this will push data into mongodb atlas via mongoose 
-                //collection will be named after your pural form of your model name
-    });
-})
-
-app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, post).then( result => {
-        console.log(result);
-        res.status(200).json({message: 'Update successful!'});
-    });
-});
-
-app.get('/api/posts',(req, res, next) => {
-    // const posts = [
-    //     {id: 'kenneth', 
-    //     title: 'First server-side post',
-    //     content: 'This is coming from the server'
-    //     },
-    //     {id: 'keen', 
-    //     title: 'Second server-side post',
-    //     content: 'This is also coming from the server!!!'
-    //     }
-    // ];
-    Post.find().then(documents => {
-            // console.log(documents);
-            const posts = documents
-
-            res.status(200).json({ // need to send response within async function
-                message: 'Post fetched successfully!',
-                posts: posts
-            });
-        });
-});
-
-app.get('/api/posts/:id',(req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post);
-        }else {
-            res.status(404).json({message: 'Post not found!'});
-        }
-    });
-});
-
-
-app.delete("/api/posts/:id", (req, res, next) => { 
-    console.log(req.params.id + " Deleted");
-    Post.deleteOne({_id: req.params.id}).then(result => {
-        console.log(result);
-        res.status(200).json({message: "Post deleted!"});
-    })
-
-    
-})
 
 module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const multer = require('multer');
+const Post = require('../models/post');
+const router = express.Router();
+
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = new Error('Invalid mime type');
+        if (isValid) {
+            error = null;
+        }
+        callback(null, 'backend/image');
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.toLowerCase().split(' ').join('-');
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        callback(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
+
+router.post('',multer(storage).single('image'), (req, res, next) => {
+    // const post = req.body;
+    
+    const post = new Post({ // Post model is from router.js where it is hooked up to mongoose
+        title: req.body.title,
+        content: req.body.content
+    });
+    // console.log(res);
+    post.save().then(postCreated => {
+        console.log(postCreated);
+        res.status(201).json({
+            message: 'Post added Successfully!',
+            postId: postCreated._id
+        }); //this will push data into mongodb atlas via mongoose 
+                //collection will be named after your pural form of your model name
+    });
+})
+
+router.put("/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    Post.updateOne({_id: req.params.id}, post).then( result => {
+        console.log(result);
+        res.status(200).json({message: 'Update successful!'});
+    });
+});
+
+router.get('',(req, res, next) => {
+    // const posts = [
+    //     {id: 'kenneth', 
+    //     title: 'First server-side post',
+    //     content: 'This is coming from the server'
+    //     },
+    //     {id: 'keen', 
+    //     title: 'Second server-side post',
+    //     content: 'This is also coming from the server!!!'
+    //     }
+    // ];
+    Post.find().then(documents => {
+            // console.log(documents);
+            const posts = documents
+
+            res.status(200).json({ // need to send response within async function
+                message: 'Post fetched successfully!',
+                posts: posts
+            });
+        });
+});
+
+router.get('/:id',(req, res, next) => {
+    Post.findById(req.params.id).then(post => {
+        if (post) {
+            res.status(200).json(post);
+        }else {
+            res.status(404).json({message: 'Post not found!'});
+        }
+    });
+});
+
+
+router.delete("/:id", (req, res, next) => { 
+    console.log(req.params.id + " Deleted");
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({message: "Post deleted!"});
+    })
+
+    
+})
+module.exports = router;
